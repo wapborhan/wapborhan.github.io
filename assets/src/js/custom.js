804ed3829c6ef4b4b76a71819abcfe63267d1f19
Borhan Uddin <68281712+borhan4238@users.noreply.github.com>
2020-11-02 02:19:51 +0600

    update

--- assets/src/js/custom.js
+++ assets/src/js/custom.js
@@ -71,3 +71,44 @@ $.each(lang, function (language, pourcent) {
     multiply++;
 
 });
+
+
+//menu scroll
+$('a').click(function () {
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500);
+    return false;
+});
+
+// Cache selectors
+var topMenu = $(".menu-center"),
+    topMenuHeight = topMenu.outerHeight() + 15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function () {
+        var item = $($(this).attr("href"));
+        if (item.length) {
+            return item;
+        }
+    });
+
+// Bind to scroll
+$(window).scroll(function () {
+    // Get container scroll position
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+
+    // Get id of current scroll item
+    var cur = scrollItems.map(function () {
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+    // Set/remove active class
+    menuItems
+        .parent().removeClass("active")
+        .end().filter("[href='#" + id + "']").parent().addClass("active");
+});
