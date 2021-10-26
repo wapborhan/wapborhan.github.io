d08b8cf2aac3741beba2062b6645fefd0f44c25b
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-10-26 12:37:58 +0600

    mouse hover effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -425,3 +425,31 @@
 
 
 })(jQuery, window)
+
+
+//Custom Mouse Hover
+document.onmousemove = animatedCircles;
+
+        const colors = ['#ccc', '#6cf', '#eba13a'];
+
+        function animatedCircles(event) {
+            let circle = document.createElement('div');
+            circle.setAttribute('class', 'circle');
+            document.body.appendChild(circle);
+
+            circle.style.left = event.clientX + 'px';
+            circle.style.top = event.clientY + 'px';
+
+            let color = colors[Math.floor(Math.random() * colors.length)];
+            circle.style.borderColor = color;
+
+            circle.style.transition = 'all 0.5s linear 0s';
+
+            circle.style.left = circle.offsetLeft - 20 + 'px';
+            circle.style.top = circle.offsetTop - 20 + 'px';
+
+            circle.style.width = '50px';
+            circle.style.height = '50px';
+            circle.style.borderWidth = '5px';
+            circle.style.opacity = 0;
+        }
