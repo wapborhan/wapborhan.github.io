c08d8713268edfbd3523b6e1a97a70a0dd141ea2
Borhan Uddin <borhaninfos@gmail.com>
2022-02-06 23:41:18 +0600

    update images

--- assets/js/main.js
+++ assets/js/main.js
@@ -479,6 +479,31 @@
 //             circle.style.opacity = 0;
 //
 
+// Back To Top Progress
+var progressPath = document.querySelector(".progress path");
+var pathLength = progressPath.getTotalLength();
+progressPath.style.transition = progressPath.style.WebkitTransition = "none";
+progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+progressPath.style.strokeDashoffset = pathLength;
+progressPath.getBoundingClientRect();
+progressPath.style.transition = progressPath.style.WebkitTransition =
+  "stroke-dashoffset 300ms linear";
+
+var updateProgress = function () {
+  // calculate values
+  var scroll = $(window).scrollTop();
+  var height = $(document).height() - $(window).height();
+  var percent = Math.round((scroll * 100) / height);
+  var progress = pathLength - (scroll * pathLength) / height;
+  // update dashOffset
+  progressPath.style.strokeDashoffset = progress;
+  // update progress count
+  $(".percent").text(percent + "%");
+};
+
+updateProgress();
+$(window).scroll(updateProgress);
+
 //hide table th
 let trAll = document.querySelectorAll("tr th:nth-child(1)");
 let trAlll = trAll.length;
