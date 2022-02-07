5d8d8baff2ddad1961b75db50f95a4521f05eb52
Borhan Uddin <borhaninfos@gmail.com>
2022-02-07 16:15:34 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -452,32 +452,10 @@
   imJs.m();
 })(jQuery, window);
 
-//Custom Mouse Hover
-// document.onmousemove = animatedCircles;
-
-//         const colors = ['#ccc', '#6cf', '#eba13a'];
-
-//         function animatedCircles(event) {
-//             let circle = document.createElement('div');
-//             circle.setAttribute('class', 'circle');
-//             document.body.appendChild(circle);
-
-//             circle.style.left = event.clientX + 'px';
-//             circle.style.top = event.clientY + 'px';
-
-//             let color = colors[Math.floor(Math.random() * colors.length)];
-//             circle.style.borderColor = color;
-
-//             circle.style.transition = 'all 0.5s linear 0s';
-
-//             circle.style.left = circle.offsetLeft - 20 + 'px';
-//             circle.style.top = circle.offsetTop - 20 + 'px';
-
-//             circle.style.width = '50px';
-//             circle.style.height = '50px';
-//             circle.style.borderWidth = '5px';
-//             circle.style.opacity = 0;
-//
+// Bootsrap ToolTip
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
 // Back To Top Progress
 var progressPath = document.querySelector(".progress path");
@@ -565,7 +543,3 @@ for (var i = 0; i < jlen; i++) {
   jsLink.setAttribute("href", "https://www.javascript.com/");
   jsLink.setAttribute("target", "_blank");
 }
-
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-});
